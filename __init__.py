d5dcb994a9c4c635f49f42d332080970aacc498d
S. M. Estiaque Ahmed <rumi@localhost.localdomain>
2019-01-12 02:50:23 +0600

    Initial commit.

---
+++ __init__.py
@@ -0,0 +1,92 @@
+# Copyright 2019 S. M. Estiaque Ahmed
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import requests
+
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill
+from mycroft.skills.core import intent_handler
+
+
+class DictionarySkill(MycroftSkill):
+    def __init__(self):
+        super(DictionarySkill, self).__init__(name="DictionarySkill")
+
+    @intent_handler(IntentBuilder("DefineIntent").require("Define")
+                    .require("Word"))
+    def handle_define_intent(self, message):
+        try:
+            config = self.config_core.get("DictionarySkill", {})
+
+            if not config == {}:
+                base_url = str(config.get("base_url"))
+                app_id = str(config.get("app_id"))
+                app_key = str(config.get("app_key"))
+                language = str(config.get("language"))
+
+            else:
+                base_url = str(self.settings.get("base_url"))
+                app_id = str(self.settings.get("app_id"))
+                app_key = str(self.settings.get("app_key"))
+                language = str(self.settings.get("language"))
+
+            self.log.error("app_key: {0}".format(app_key))
+
+            if not base_url or not app_id or not app_key or not language:
+                raise Exception("None found.")
+
+        except Exception as e:
+            self.speak_dialog("settings.error")
+            self.log.error(e)
+            return
+
+        word = message.data.get("Word")
+
+        self.log.error("Word: {0}".format(word))
+
+        if not word:
+            return
+
+        api_url = "{0}/entries/{1}/{2}".format(base_url, language, word)
+
+        api_headers = {'Content-Type': 'application/json',
+                       'Accept': 'application/json',
+                       'app_id': app_id,
+                       'app_key': app_key}
+
+        try:
+            response = requests.get(api_url, headers=api_headers)
+
+            if response.status_code == 200:
+                definition = response.json()['results'][0][
+                                                'lexicalEntries'][0][
+                                                    'entries'][0]['senses'][0][
+                                                        'definitions'][0]
+
+                self.speak_dialog("definition", {"definition": definition})
+
+            elif response.status_code == 404:
+                self.speak_dialog("invalid", {"word": word})
+
+        except Exception as e:
+            self.speak_dialog("connection.error")
+            self.log.error(e)
+            return
+
+    def stop(self):
+        pass
+
+
+def create_skill():
+    return DictionarySkill()
